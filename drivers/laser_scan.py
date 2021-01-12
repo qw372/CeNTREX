97eaa93b9e7e18eb72104975b5da3cdd9d7f4e1c
qw372 <qian.wang.qw95@yale.edu>
2021-01-12 17:35:44 -0500

    laser scan

--- drivers/laser_scan.py
+++ drivers/laser_scan.py
@@ -3,6 +3,7 @@ import time
 import logging
 import socket
 import struct
+import configparser
 
 class laser_scan:
     def __init__(self, time_offset, *constr_param):
@@ -17,21 +18,24 @@ class laser_scan:
         # each element in self.warnings should be in format: [time.time()-self.time_offset, "warning content"]
         self.warnings = []
 
+        # HDF attributes generated when constructor is run
+        self.new_attributes = []
+
         # make use of the constr_param
         self.constr_param = constr_param
         print(f"Constructor got passed the following parameter: {self.constr_param}")
 
         server_addr = configparser.ConfigParser()
-        server_addr.read(r"C:\Users\DeMille Group\github\SrF-lab-control-accessory\Lasers\server_address.ini")
+        server_addr.read(r"C:\Users\qw95\github\SrF-lab-control\device_accessories\Lasers\server_address.ini")
         self.host = server_addr["Server address"]["host"].strip()
-        self.port = float(server_addr["Server address"]["port"])
+        self.port = int(server_addr["Server address"]["port"])
 
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
             self.sock.connect((self.host, self.port))
             self.verification_string = "laser"
         except Exception as err:
-            print("laser scan: TCP connection failed.")
+            print(f"laser scan: TCP connection failed. \n{err}")
             self.verification_string = "fail"
 
     def __enter__(self):
@@ -40,7 +44,10 @@ class laser_scan:
 
     def __exit__(self, *exc):
         # when with...as... statementn finished running, __exit__ will be called
-        pass
+        try:
+            self.sock.close()
+        except Exception:
+            pass
 
     def ReadValue(self):
         # self.warnings.append([time.time()-self.time_offset, "warning test 2"])
@@ -50,19 +57,21 @@ class laser_scan:
                ]
 
     def scan(self, type, val):
-        if type = "laser0(MHZ)":
+        if type == "laser0(MHZ)":
             self.update_laser0_freq(val)
-        elif type = "laser1(MHZ)":
+        elif type == "laser1(MHZ)":
             self.update_laser1_freq(val)
         else:
             print("laser scan: scan type not supported.")
 
     def update_laser0_freq(self, freq):
         self.laser_freq[0] = float(freq)
+        # print(self.laser_freq)
         bytearray = struct.pack('>H', 0)
         bytearray += struct.pack('>d', float(self.laser_freq[0]))
         self.sock.sendall(bytearray)
         re = self.sock.recv(1024)
+        # print(re)
 
     def update_laser1_freq(self, freq):
         self.laser_freq[1] = float(freq)
@@ -75,3 +84,7 @@ class laser_scan:
         warnings = self.warnings
         self.warnings = []
         return warnings
+
+# a = laser_scan(0, 100, 200)
+# a.update_laser0_freq(200)
+# a.__exit__()
